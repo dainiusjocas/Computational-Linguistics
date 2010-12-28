09f0a192d452036469e5c58e53ad5076c6911abf
Dainius Jocas <dainiusjocas@gmail.com>
2010-12-28 01:41:14 +0100

    stoped working with problem of speed while gitting context of bigram

--- lib/collocations.rb
+++ lib/collocations.rb
@@ -11,36 +11,61 @@
 class Collocations
   attr_accessor :bigrams_with_count,    # key: bigram, value: count in corpus
     :bigrams_with_chi_square_values,    # key: bigram, value: chi-square value of bigram
+    :bigrams_with_location_in_file,     # key: bigram, value: array of [xml_id + sentence_nr]
     :startings_of_bigrams_with_count,   # key: first word of the bigram with type, value: count in corpus
     :endings_of_bigrams_with_count,     # key: second word of the bigram with type, value: count in corpus
     :bigrams,                           # array of bigrams
-    :total_number_of_bigrams
+    :total_number_of_bigrams,
+    :context_of_the_bigram,             #
+    :xml_document
   attr_reader :noun, :verb, :adjective
 
   def initialize
     @bigrams_with_count = Hash.new
     @bigrams_with_chi_square_values = Hash.new
+    @bigrams_with_location_in_file = Hash.new
     @startings_of_bigrams_with_count = Hash.new
     @endings_of_bigrams_with_count = Hash.new
     @total_number_of_bigrams = 0
     @noun = "NN"
     @verb = "VB"
     @adjective = "JJ"
+    @xml_document = nil
+    @pp = 0
   end
 
   # Adds a pair of words to the bigrams_with_count of collocations.
   # Value of the hash is an array of strings where every string is head_tail of
   # collocation
   #
-  # @param head of collocation in format [word/type]
-  # @param tail of collocation in format [word/type]
+  # @param starting of collocation in format [word/type]
+  # @param ending of collocation in format [word/type]
   #
-  def add_pair head, tail
-    pair = head + "_" + tail
+  def add_pair starting, ending
+    pair = starting + "_" + ending
     if @bigrams_with_count.has_key?(pair)
       @bigrams_with_count[pair] = @bigrams_with_count[pair] + 1
     else
       @bigrams_with_count[pair] = 1
+    end
+  end
+
+  # This method builds an hash where key is a bigram and value is an array of
+  # location description of bigram. This is needed because bigram can occur in
+  # various contexts and this is a requirement.
+  #
+  # @param starting of collocation in format [word/type]
+  # @param ending of collocation in format [word/type]
+  # @param xml_id
+  # @param sentence_nr
+  #
+  def keep_track_in_the_file starting, ending, xml_id, sentence_nr
+    pair = starting + "_" + ending
+    location = "#{xml_id}_#{sentence_nr}"
+    if @bigrams_with_location_in_file.has_key?(pair)
+      @bigrams_with_location_in_file[pair].push(location)
+    else
+      @bigrams_with_location_in_file[pair] = [location]
     end
   end
 
@@ -57,9 +82,10 @@ class Collocations
     end
   end
 
-  # gets count of the starting
+  # gets count of the specified starting of bigram
   #
   # @param starting format: [word/type]
+  #
   def get_starting_count starting
     count =  @startings_of_bigrams_with_count[starting]
     if (nil != count)
@@ -90,6 +116,7 @@ class Collocations
   # gets count of the ending
   #
   # @param ending format: [word/type]
+  #
   def get_ending_count ending
     count = @endings_of_bigrams_with_count[ending]
     if (nil != count)
@@ -114,12 +141,14 @@ class Collocations
   #
   # @param sentence string of words [word/type]
   #
-  def get_collocations_from_sentence sentence
+  def get_collocations_from_sentence sentence, xml_id, sentence_nr
     words = sentence.split " "
     for i in 0..words.size - 2
       for j in i+1..words.size - 1
         if "NN" == (words[j].split "/")[1]
           add_pair words[i], words[j]
+          keep_track_in_the_file words[i], words[j], xml_id, sentence_nr
+          get_context_of_bigram words[i], words[j], xml_id, sentence_nr
           add_starting words[i]
           add_ending words[j]
         end
@@ -133,20 +162,25 @@ class Collocations
   # @param xml xml file
   #
   def build_collocations_from_xml xml
-    xml.elements.each("//s") {
-      |item|
-      sentence = ""
-      item.elements.each("child::node()") {
-        |item2|
-        if (item2.text == ".")
-          get_collocations_from_sentence sentence
-          sentence = ""
-        end
-        if (item2.attributes['type'] == @noun || item2.attributes['type'] == @verb || item2.attributes['type'] == @adjective)
-          sentence += item2.text.downcase + "/" + item2.attributes['type'] + " "
-        end
+    @xml_document = xml
+    xml.elements.each("//text") {
+      |text_node| # corpus xml can be divided into smaller xml files
+      text_node.elements.each("//s") {
+        |item|
+        sentence = ""
+        xml_id = item.elements['ancestor::text'].attributes['xml:id']
+        item.elements.each("child::node()") {
+          |item2|
+          if (item2.text == ".")
+            get_collocations_from_sentence sentence, text_node.attributes['xml:id'], item.attributes['n']
+            sentence = ""
+          end
+          if (item2.attributes['type'] == @noun || item2.attributes['type'] == @verb || item2.attributes['type'] == @adjective)
+            sentence += item2.text.downcase + "/" + item2.attributes['type'] + " "
+          end
+        }
+        get_collocations_from_sentence sentence, text_node.attributes['xml:id'], item.attributes['n']
       }
-      get_collocations_from_sentence sentence
     }
 #    sort_collocations_by_frequency 
 #    sort_startings_by_frequency
@@ -165,8 +199,8 @@ class Collocations
   # build a hash where for every bigram is assigned chi-square value
   #
   def count_chi_square
-    bigrams.each { |i|
-      @bigrams_with_chi_square_values[i] = get_chi_square_expected_value i
+    @bigrams.each { |bigram|
+      @bigrams_with_chi_square_values[bigram] = get_chi_square_expected_value bigram
     }
   end
 
@@ -194,8 +228,9 @@ class Collocations
     return bigram.split("_")[1]
   end
 
-  # file than
   # Sorts hash of collocations by occurence count
+  #
+  # @return array of arrays where second level array is [bigram, frequency]
   #
   def sort_collocations_by_frequency
     @bigrams_with_count = @bigrams_with_count.sort{|a,b| a[1]<=>b[1]}.reverse
@@ -203,31 +238,62 @@ class Collocations
 
   # Sorts hash of startings of bigrams_with_count by occurence count
   #
+  # @return array of arrays where second level array is [starting, frequency]
+  #
   def sort_startings_by_frequency
     @startings_of_bigrams_with_count = @startings_of_bigrams_with_count.sort{|a,b| a[1]<=>b[1]}.reverse
   end
 
   # Sorts hash of endings of bigrams_with_count by occurence count
   #
+  # @return array of arrays where second level array is [ending, frequency]
+  #
   def sort_endings_by_frequency
     @endings_of_bigrams_with_count = @endings_of_bigrams_with_count.sort{|a,b| a[1]<=>b[1]}.reverse
   end
 
-  #Prints first n elements of bigrams_with_count
+  # Prints first n elements of bigrams_with_count
+  # search results in results/frequency.txt
   #
   def print_n_most_frequent_bigrams n
+    out = File.new("../results/frequency.txt", "w+")
     array_of_most_frequent_bigrams = @bigrams_with_count.sort{|a,b| a[1]<=>b[1]}.reverse
-    for i in 0..n
-      puts "#{array_of_most_frequent_bigrams[i][0]} #{array_of_most_frequent_bigrams[i][1]}"
+    for i in 0..n-1
+      out.puts "#{array_of_most_frequent_bigrams[i][0]} #{array_of_most_frequent_bigrams[i][1]}"
     end
+    out.close
   end
 
-  #Prints first n elements of bigrams_with_count
+  # Prints first n elements of bigrams_with_count with chi-square
+  # search results in file results/chi-square.txt
   #
   def print_n_bigrams_with_biggest_chi_square_value n
+    out = File.new("../results/chi-square.txt", "w+")
     array_of_bigrams_with_descending_chi_square_value = @bigrams_with_chi_square_values.sort{|a,b| a[1]<=>b[1]}.reverse
-    for i in 0..n
-      puts "#{array_of_bigrams_with_descending_chi_square_value[i][0]} #{array_of_bigrams_with_descending_chi_square_value[i][1]}"
+    for i in 0..n-1
+      out.puts "#{array_of_bigrams_with_descending_chi_square_value[i][0]} #{array_of_bigrams_with_descending_chi_square_value[i][1]}"
     end
+    out.close
+  end
+
+  # Method which get context of collocation
+  # I will take approximately 200 words
+  #
+  # @param bigram
+  #
+  def get_context_of_bigram starting, ending, xml_id, sentence_nr
+    #amount_of_words = 0
+    #delta = 1
+#    sentence = @xml_document.elements["descendant::text[attribute::xml:id='#{xml_id}']"]
+#    sentence = sentence.elements["descendant::s[attribute::n='#{sentence_nr}']"]
+    begin
+      #puts sentence
+      #sentence.elements.each("child::w") { |word| amount_of_words += 1} if amount_of_words < 200
+      #sentence = text_node.elements["descendant::s[attribute::n=#{sentence_nr.to_i + delta}]"]
+
+#      puts sentence.attributes['n']#"#{sentence_nr} #{@pp}"
+#      @pp += 1
+      #amount_of_words += 1
+    end #while amount_of_words < 21
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -14,23 +14,20 @@ require 'collocations.rb'
 include REXML
 
 
-puts "===The beginning of the program"
+puts "The beginning of the program============================================="
 collocations = Collocations.new
 xml = XMLTools.new "../test/a01.xml"
 xml = xml.get_xml_document
 collocations.build_collocations_from_xml(xml)
-collocations.count_chi_square
-puts "The most frequent bigrams"
-collocations.print_n_most_frequent_bigrams(100)
-puts "========================================================================="
-puts "Collocations with largest chi-square values"
-collocations.print_n_bigrams_with_biggest_chi_square_value 100
-puts "========================================================================="
 
-#corpus = Corpus.new "../test/a01.xml"
-#corpus.extract_collocations
-#
-#corpus.collocations.print_bigrams_with_count
-#puts corpus.get_array_of_words_from_sentence(0)
-puts
-puts "===The end of the program"
+#collocations.bigrams_with_location_in_file.each { |key, value|
+#  out.puts "#{key} #{value}"
+#}
+#collocations.count_chi_square
+#puts "The most frequent bigrams"
+#collocations.print_n_most_frequent_bigrams(1000)
+#puts "========================================================================="
+#puts "Collocations with largest chi-square values"
+#collocations.print_n_bigrams_with_biggest_chi_square_value 1000
+
+puts "The end of the program==================================================="
